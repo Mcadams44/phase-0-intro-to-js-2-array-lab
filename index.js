01b74d62d2a89a9a4e29e3b9a8ebb83b3367b67b
Mcadams <mcadamsproject@gmail.com>
2025-06-14 15:16:15 +0300

    rema

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+// 1. Declare and initialize the global `cats` array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions (modify original array)
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions (return new arrays)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
